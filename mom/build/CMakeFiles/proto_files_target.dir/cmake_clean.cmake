d6cb7b950ce4cfabc00914b68ed7b633e91a3548
camazog1 <camazog1@eafit.edu.co>
2025-04-20 20:48:57 -0500

    addition of CMakeLists.txt

---
+++ mom/build/CMakeFiles/proto_files_target.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/carlosm/Documentos/Projects/Proyecto1_TopicosTelematica/mom/include/mom.grpc.pb.cc"
+  "/home/carlosm/Documentos/Projects/Proyecto1_TopicosTelematica/mom/include/mom.grpc.pb.h"
+  "/home/carlosm/Documentos/Projects/Proyecto1_TopicosTelematica/mom/include/mom.pb.cc"
+  "/home/carlosm/Documentos/Projects/Proyecto1_TopicosTelematica/mom/include/mom.pb.h"
+  "CMakeFiles/proto_files_target"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/proto_files_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
